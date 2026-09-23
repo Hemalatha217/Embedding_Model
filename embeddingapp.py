bf463822fc15e5c1cfefb30e1d27677f2e329c2e
Hemalatha217 <hh4888125@gmail.com>
2026-09-23 14:42:10 +0530

    Update embeddingapp.py

--- embeddingapp.py
+++ embeddingapp.py
@@ -1,18 +1,13 @@
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 
-# Load model
 model = SentenceTransformer("all-MiniLM-L6-v2")
-
-# Input two sentences
 sentence1 = input("Enter Sentence 1: ")
 sentence2 = input("Enter Sentence 2: ")
 
-# Generate embeddings
 embedding1 = model.encode(sentence1)
 embedding2 = model.encode(sentence2)
 
-# Display embeddings
 print("\n===== EMBEDDINGS =====")
 print("\nSentence 1:", sentence1)
 print("Embedding 1:", embedding1)
@@ -22,17 +17,14 @@ print("\nSentence 2:", sentence2)
 print("Embedding 2:", embedding2)
 print("Dimension:", len(embedding2))
 
-# Calculate similarity
 similarity = cosine_similarity(
     [embedding1],
     [embedding2]
 )
 
-# Display similarity score
 print("\n===== SIMILARITY SCORE =====")
 print("Similarity:", similarity[0][0])
 
-# Interpretation
 if similarity[0][0] > 0.8:
     print("The sentences are very similar.")
 elif similarity[0][0] > 0.5:
